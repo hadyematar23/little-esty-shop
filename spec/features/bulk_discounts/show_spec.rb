c02ea6f7ee8874e7a3a069b82553a747c13a09ba
Huy Phan <huyphan520@gmail.com>
2023-03-03 19:40:07 -0600

    User story 1 and 3

---
+++ spec/features/bulk_discounts/show_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe '/bulk_discounts/:id' do
+  describe 'When I vist the bulk discount show page' do
+    before do
+      @merchant2 = Merchant.create!(name: "Hady's Beach Shack")
+
+      @discounts1 = @merchant2.bulk_discounts.create(title: "Small Discount", quantity_threshold: 5, percentage_discount: 20.00)
+      @discounts2 = @merchant2.bulk_discounts.create(title: "Big Discount", quantity_threshold: 10, percentage_discount: 30.00)
+    end
+
+    it 'I see the bulk discount info' do
+      visit bulk_discount_path(@discounts1)
+
+      expect(page).to have_content(@discounts1.title)
+      expect(page).to have_content(@discounts1.quantity_threshold)
+      expect(page).to have_content(@discounts1.percentage_discount)
+    end
+  end
+end
